25f363f6da44ae575d848d484239c0e6b6d9c8b9
Joao Pedro <jpresende502@gmail.com>
2021-02-28 19:02:12 -0300

    Adicionando feature de editar e remover uma receita.

--- src/app/models/Receitas.js
+++ src/app/models/Receitas.js
@@ -38,5 +38,37 @@ module.exports = {
             if (err) throw `Database error. ${err}`;
             callback(results.rows[0])
         })
+    },
+    update(data, callback) {
+        const query = `
+        UPDATE recipes SET
+            title=($1),
+            image_url=($2),
+            ingredients=($3),
+            prepare=($4),
+            informations=($5)
+        WHERE id = $6
+        `
+        const values = [
+            data.title,
+            data.image_url,
+            Array(data.ingredients),
+            Array(data.prepare),
+            data.informations,
+            data.id
+        ]
+        
+        db.query(query, values, (err, _) => {
+            if(err) throw `Database error. ${err}`
+
+            callback()
+        })
+    },
+    delete(id, callback) {
+        db.query(`DELETE FROM recipes WHERE id = $1`, [id], (err, _) => {
+            if (err) throw `Database error. ${err}`
+
+            callback()
+        })
     }
 }
