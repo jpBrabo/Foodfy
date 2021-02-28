8742286f18cd526073f9057f549b1ed7e71043aa
Joao Pedro <jpresende502@gmail.com>
2021-02-28 19:01:19 -0300

    Criando routes.js

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const receitas = require("./app/controllers/receitas")
+const routes = express.Router()
+
+// - Configurando rotas
+routes.get("/", receitas.index)
+routes.get("/sobre", receitas.about)
+routes.get("/receitas", receitas.allRecipes)
+routes.get("/receitas/:id", receitas.show)
+routes.get("/admin/receitas", receitas.adminRecipes)
+routes.get("/admin/create",receitas.adminCreate)
+routes.get("/admin/edit/:id", receitas.adminEdit)
+routes.post("/receitas", receitas.post)
+routes.put("/receitas", receitas.put)
+routes.delete("/receitas", receitas.delete)
+
+module.exports = routes
