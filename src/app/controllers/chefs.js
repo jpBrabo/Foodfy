21db9e5fb20dfbbe1bd4f2d9164ce6df7791898d
Joao Pedro <jpresende502@gmail.com>
2021-03-02 23:05:13 -0300

    Ajustando controllers

---
+++ src/app/controllers/chefs.js
@@ -0,0 +1,72 @@
+const Chefs = require("../models/Chefs")
+
+module.exports = {
+    // - Controle de rotas dos chefs
+    allChefs(req,res){
+        Chefs.allChefs(chefs => {
+            return res.render("chefs/chefs", { chefs })
+        })
+    },
+    showChef(req,res) {
+        Chefs.findChef(req.params.id, chef => {
+            if(!chef) throw new Error("Recipe not found.")
+            return res.render("chefs/show", { chef })
+        })
+    },
+    adminChef(req,res) {
+        Chefs.allChefs(chefs => {
+            return res.render("admin/chef", { chefs })
+        })
+    },
+    adminChefShow(req,res) {
+        Chefs.findChef(req.params.id, chef => {
+            return res.render("admin/chef_view", { chef })
+        })
+    },
+    adminChefEdit(req,res) {
+        Chefs.findChef(req.params.id, chef => {
+            if(!chef) throw new Error("Recipe not found.")
+            return res.render("chefs/edit", { chef })
+        })
+    },
+    adminChefCreate(req,res) {
+        return res.render("chefs/create")
+    },
+    postChef(req,res) {
+        const keys = Object.keys(req.body)
+        //req.body.objeto
+        for(key of keys){
+            if(req.body[key] == '')
+                return res.send("Please, fill all fields!!! >:(")
+        }
+        Chefs.createChef(req.body, chefs => {
+            return res.redirect(`/chefs/${chefs.id}`)
+        })
+    },
+    putChef(req,res) {
+        const keys = Object.keys(req.body);
+        console.log(req.body)
+        for (key of keys) {
+          if (req.body[key] == "") {
+            return res.send("Please, fill all fields!");
+          }
+        }
+
+        Chefs.updateChef(req.body, () => {
+            return res.redirect(`/admin/chefs/view/${req.body.id}`)
+        })
+    },
+    deleteChef(req,res) {
+        const keys = Object.keys(req.body);
+
+        for (key of keys) {
+          if (req.body[key] == "") {
+            return res.send("Please, fill all fields!");
+          }
+        }
+        
+        Chefs.deleteChef(req.body.id, () => {
+            return res.redirect(`/admin/chefs`)
+        })
+    },
+}
