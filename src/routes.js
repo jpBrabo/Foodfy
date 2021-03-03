7697b753e853e0d3b899382e951476c714c9a825
Joao Pedro <jpresende502@gmail.com>
2021-03-02 23:04:24 -0300

    Adicionando novas rotas.

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,33 @@
 const express = require("express")
 const receitas = require("./app/controllers/receitas")
+const chefs = require("./app/controllers/chefs")
 const routes = express.Router()
 
 // - Rotas da aplicação.
 routes.get("/", receitas.index)
 routes.get("/sobre", receitas.about)
+
+// - Rotas das receitas
 routes.get("/receitas", receitas.allRecipes)
-routes.get("/receitas/:id", receitas.show)
-routes.get("/admin/receitas", receitas.adminRecipes)
-routes.get("/admin/chef", receitas.adminChef)
-routes.get("/admin/create",receitas.adminCreate)
-routes.get("/admin/edit/:id", receitas.adminEdit)
-routes.post("/receitas", receitas.post)
-routes.put("/receitas", receitas.put)
-routes.delete("/receitas", receitas.delete)
+routes.get("/receitas/:id", receitas.showRecipe)
+routes.get("/admin/recipes", receitas.adminRecipes)
+routes.get("/admin/recipes/create",receitas.adminRecipeCreate)
+routes.get("/admin/recipes/edit/:id", receitas.adminRecipeEdit)
+
+routes.post("/receitas", receitas.postRecipe)
+routes.put("/receitas", receitas.putRecipe)
+routes.delete("/receitas", receitas.deleteRecipe)
+
+// - Rotas dos chefs
+routes.get("/chefs", chefs.allChefs)
+routes.get("/chefs/:id", chefs.showChef)
+routes.get("/admin/chefs", chefs.adminChef)
+routes.get("/admin/chefs/view/:id", chefs.adminChefShow)
+routes.get("/admin/chefs/create",chefs.adminChefCreate)
+routes.get("/admin/chefs/edit/:id", chefs.adminChefEdit)
+
+routes.post("/chefs", chefs.postChef)
+routes.put("/chefs", chefs.putChef)
+routes.delete("/chefs", chefs.deleteChef)
 
 module.exports = routes
