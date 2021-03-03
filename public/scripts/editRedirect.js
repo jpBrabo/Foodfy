e4c1330e78ec71b9200c7a85ae85b9c50db9b3e7
Joao Pedro <jpresende502@gmail.com>
2021-03-02 23:04:36 -0300

    editando scripts.

--- public/scripts/editRedirect.js
+++ public/scripts/editRedirect.js
@@ -3,6 +3,6 @@ const btnElement = document.querySelector("button")
 function editRedirect(){
     var idElement = document.querySelector(".receita")
     var id = idElement.getAttribute("id")
-    window.location.href = `/admin/edit/${id}`
+    window.location.href = `/admin/recipes/edit/${id}`
 }
 btnElement.onclick = editRedirect
