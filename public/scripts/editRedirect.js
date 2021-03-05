9da74233d4b00e06fb6a19f1d7c0f76251220cc3
Joao Pedro <jpresende502@gmail.com>
2021-03-04 22:01:44 -0300

    Deletando script.

--- public/scripts/editRedirect.js
+++
@@ -1,8 +0,0 @@
-const btnElement = document.querySelector("button")
-
-function editRedirect(){
-    var idElement = document.querySelector(".receita")
-    var id = idElement.getAttribute("id")
-    window.location.href = `/admin/recipes/edit/${id}`
-}
-btnElement.onclick = editRedirect
